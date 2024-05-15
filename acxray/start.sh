73026bde351a3d8316969a33a46613ab2b15dc37
coomsn <61256281+coomsn@users.noreply.github.com>
2024-05-15 23:13:59 +0800

    Update start.sh

--- acxray/start.sh
+++ acxray/start.sh
@@ -1,20 +1,20 @@
 #!/system/bin/sh
 MODDIR="/data/adb/modules/acxray-module"
-SCRIPTS_DIR="/data/adb/acxray/xray.inotify"
+SCRIPTS_DIR="/data/adb/acxray" #####
 busybox="/data/adb/magisk/busybox"
 normal="\033[0m"
 green="\033[0;32m"
 red="\033[91m"
-
 cd ${0%/*}
-
 source ./xray.service
+
+inot_gid=20001
 
 if [ ! -f ${MODDIR}/disable ]; then
 	run
 fi
 if pgrep inotifyd > /dev/null 2>&1 ; then
-  pkill -g 20000
+  pkill -g ${inot_gid}
 fi
-${busybox} setuidgid 0:20000 inotifyd "${SCRIPTS_DIR}" "${MODDIR}" > /dev/null 2>&1 &
+${busybox} setuidgid 0:${inot_gid} inotifyd "${SCRIPTS_DIR}/xray.inotify" "${MODDIR}" > /dev/null 2>&1 &
 echo -e "${green}now,xray.inotify is running with PID ${red}$(pgrep inotifyd)${green}.${normal}"
